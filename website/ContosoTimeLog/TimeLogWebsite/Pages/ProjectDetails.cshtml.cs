df3cb6b099b5c4e8f09576a238ae98905452997c
Scott Berry <scott.berry@microsoft.com>
2025-04-01 16:45:28 +1100

    Added code for sample website

---
+++ website/ContosoTimeLog/TimeLogWebsite/Pages/ProjectDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TimeLogWebsite.Pages
+{
+    public class ProjectDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
